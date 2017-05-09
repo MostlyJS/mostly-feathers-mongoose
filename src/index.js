3f206fd329f4a4685751e25129f8fec2c0d2faad
forafish <fora.luo@gmail.com>
2017-05-09 14:20:42 +0800

    Remove console

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import * as hooks from './hooks';
 import * as plugins from './plugins';
 import service from './service';
-console.log("####1", hooks);
-console.log("####2", plugins);
-console.log("####3", service);
 
 Object.assign(service, { hooks, plugins, service });
 
