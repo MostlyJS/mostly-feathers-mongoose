cbdc30686574249c902c8c9e5b13e0b7fe3fb8c1
forafish <fora.luo@gmail.com>
2017-06-15 05:17:16 +0800

    Add service action support

--- src/service.js
+++ src/service.js
@@ -14,14 +14,14 @@ export class Service extends BaseService {
   }
 
   find(params) {
-    // external call
+    // default behaviours for external call
     if (params.provider && params.query) {
       // fix id query as _ids
       if (params.query.id) {
         params.query._id = params.query.id;
         delete params.query.id;
       }
-      // filter destroyed item
+      // filter destroyed item by default
       if (!params.query.destroyedAt) {
         params.query.destroyedAt = null;
       }
@@ -30,15 +30,14 @@ export class Service extends BaseService {
         params.query.$sort = { createdAt: -1 };
       }
     }
-    if (params.softDelete) {
-      params.query.destroyedAt = null;
-    }
-    // allow params for after hook usage
+
+    // allow extra params to be used only in after hook
     if (params.query && params.query.$after) {
       params.after = params.query.$after;
       delete params.query.$after;
     }
-    // search
+
+    // search by regex
     Object.keys(params.query || []).forEach(field => {
       if (params.query[field] && params.query[field].$search && field.indexOf('$') === -1) {
         params.query[field] = { $regex: new RegExp(params.query[field].$search), $options: 'i' };
@@ -47,20 +46,31 @@ export class Service extends BaseService {
         params.query[field] = { $regex: new RegExp(params.query[field].$like), $options: 'i' };
       }
     });
-    //debug('service %s find', this.name, params);
-    debug('---------- service %s find -----------', this.name);
-    return super.find(params);
+
+    debug('service %s find %j', this.name, params.query);
+
+    const action = params.__action;
+    if (!action || action === 'find') {
+      return super.find(params);
+    }
+
+    if (action === 'count') {
+      delete params.__action;
+      return this.count(params);
+    } else {
+      throw new Error("No such **find** action: " + action);
+    }
   }
 
   get(id, params) {
-    const action = params.action;
+    const action = params.__action;
     if (!action || action === 'get') {
-      debug('---------- service %s get %j -----------', this.name, id);
+      debug('service %s get %j', this.name, id);
       return super.get(id, params);
     }
     
     if (this[action]) {
-      delete params.action;
+      delete params.__action;
       return this.action(action, id, {}, params);
     }
     throw new Error("No such **get** action: " + action);
@@ -82,65 +92,78 @@ export class Service extends BaseService {
   }
 
   update(id, data, params) {
-    if (!id) throw new Error("Mutiple update is prohibit now.");
-    const action = params.action;
+    if (!id && !params.query.$mutiple) throw new Error("Mutiple update is prohibit now.");
+
+    const action = params.__action;
     if (!action || action === 'update') {
       return this.validate(data).then(() => {
+        delete params.query.$mutiple;
         return super.update(id, data, params);
       });
     }
-    if (this[action]) {
-      delete params.action;
+    
+    if (!this[action]) {
+      delete params.__action;
       return this.action(action, id, data, params);
+    } else {
+      throw new Error("No such **put** action: " + action);
     }
-    throw new Error("No such **put** action: " + action);
   }
 
   patch(id, data, params) {
-    if (!id) throw new Error("Mutiple patch is prohibit now.");
-    const action = params.action;
+    if (!id && !params.query.$mutiple) throw new Error("Mutiple patch is prohibit now.");
+
+    const action = params.__action;
     if (!action || action === 'patch') {
+      delete params.query.$mutiple;
       return super.patch(id, data, params);
     }
+    debug('#####', action, this[action]);
     if (this[action]) {
-      delete params.action;
+      delete params.__action;
       return this.action(action, id, data, params);
+    } else {
+      throw new Error("No such **patch** action: " + action);
     }
-    throw new Error("No such **patch** action: " + action);
   }
 
   remove(id, params) {
-    if (!id) throw new Error("Mutiple remove is prohibit now.");
-    if (params.query && params.query.$force) {
-      delete params.query.$force;
-      return super.remove(id, params);
-    } else {
-      const data = { destroyedAt: new Date() };
-      return super.patch(id, data, params);
+    if (!id && !params.query.$mutiple) throw new Error("Mutiple remove is prohibit now.");
+    
+    const action = params.__action;
+    if (!action || action === 'remove') {
+      if (params.query.$force) {
+        delete params.query.$mutiple;
+        delete params.query.$force;
+        return super.remove(id, params);
+      } else {
+        const data = { destroyedAt: new Date() };
+        return super.patch(id, data, params);
+      }
     }
-  }
 
-  restore(id, params) {
-    const data = { destroyedAt: null };
-    return super.patch(id, data, params);
+    if (action === 'restore') {
+      delete params.__action;
+      return this.restore(id, params);
+    } else {
+      throw new Error("No such **remove** action: " + action);
+    }
   }
 
   action(action, id, data, params) {
     // delete params.provider;
     return this.get(id, params).then(origin => {
       origin = origin.data || origin;
-      if (!origin) throw new Error('No such record ' + id + ' in ' + this.Model.modelName);
+      if (!origin) {
+        throw new Error('No such record ' + id + ' in ' + this.Model.modelName);
+      }
       return this[action].call(this, id, data, params, origin);
     });
   }
-  
-  aggregate(params) {
-    if(params && params.query && params.query.aggregate) {
-      return this.Model.aggregate(params.query.aggregate).exec();
-    }
-    else {
-      throw new Error("Invalid aggregate params");
-    }
+
+  restore(id, params) {
+    const data = { destroyedAt: null };
+    return super.patch(id, data, params);
   }
 
   count(params) {
