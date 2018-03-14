50de3d5bbc998d4998c5583f940597c0cd70607b
forafish <fora.luo@gmail.com>
2018-03-14 16:06:30 +0800

    cache with hook key

--- src/hooks/cache.js
+++ src/hooks/cache.js
@@ -1,28 +1,33 @@
 import makeDebug from 'debug';
 import fp from 'mostly-func';
-import { getItems } from 'feathers-hooks-common';
+import { getHookDataAsArray, genHookKey } from '../helpers';
 
 const debug = makeDebug('mostly:feathers-mongoose:hooks:cache');
 
 const defaultOptions = {
   idField: 'id',
+  perUser: false
 };
 
 export default function (cacheMap, opts) {
-  opts = Object.assign({}, defaultOptions, opts);
+  opts = fp.assign(defaultOptions, opts);
   
   return context => {
     const idName = opts.idField || (context.service || {}).id;
     const svcName = (context.service || {}).name;
-    let items = getItems(context);
-    items = Array.isArray(items) ? items : [items];
+    const keyPrefix = genHookKey(context, opts.perUser) + ':';
+
+    const items = getHookDataAsArray(context);
 
     if (context.type === 'after') {
       if (context.method === 'remove') return;
 
       items.forEach(item => {
-        debug(`>> ${svcName} service set cache`, item[idName]);
-        cacheMap.set(item[idName], fp.clone(item));
+        const key = keyPrefix + item[idName];
+        if (!fp.contains(key, context.cacheHits || [])) {
+          debug(`>> ${svcName} service set cache`, key);
+          cacheMap.set(key, fp.clone(item));
+        }
       });
 
       return;
@@ -31,25 +36,33 @@ export default function (cacheMap, opts) {
     switch (context.method) {
       case 'find': // fall through
       case 'create':
-        return;
+        break;
       case 'get': {
-        let value = cacheMap.get(context.id);
+        const key = keyPrefix + context.id;
+        const value = cacheMap.get(key);
         if (value) {
-          debug(`<< ${svcName} service get cache`, context.id);
+          debug(`<< ${svcName} service hit cache`, key);
+          context.cacheHits = fp.concat(context.cached || [], [key]);
           context.result = value;
+        } else {
+          debug(`<< ${svcName} service miss cache`, key);
         }
-        return context;
+        break;
       }
-      default: // update, patch, remove
+      default: { // update, patch, remove
         if (context.id) {
-          cacheMap.delete(context.id);
-          return;
+          const key = keyPrefix + context.id;
+          debug(`>> ${svcName} service delete cache`, key);
+          cacheMap.delete(key);
+        } else {
+          items.forEach(item => {
+            const key = keyPrefix + context.id;
+            debug(`>> ${svcName} service delete cache`, key);
+            cacheMap.delete(key);
+          });
         }
-
-        items.forEach(item => {
-          debug(`>> ${svcName} service delete cache`, item[idName]);
-          cacheMap.delete(item[idName]);
-        });
+      }
     }
+    return context;
   };
 }
