5371edc9770bc1865b428b82878820296f2e43ae
forafish <fora.luo@gmail.com>
2018-03-15 01:09:18 +0800

    cache for find with id

--- src/hooks/cache.js
+++ src/hooks/cache.js
@@ -18,6 +18,16 @@ export default function (cacheMap, opts) {
     const altKey = genHookKey(context, opts.perUser);
 
     const items = getHookDataAsArray(context);
+    const resultFor = (data) => {
+      return {
+        metadata: {
+          total: data.length,
+          limit: context.params.query.$limit || 10,
+          skip: context.params.query.$skip || 0
+        },
+        data: data
+      };
+    };
 
     if (context.type === 'after') {
       if (context.method === 'remove') return;
@@ -31,21 +41,50 @@ export default function (cacheMap, opts) {
         }
       });
 
+      if (context.cacheResults) {
+        const results = getHookDataAsArray(context);
+        context.result = resultFor(fp.concat(context.cacheResults, results));
+      }
+
       return;
     }
 
+    const getFromCache = (id) => {
+      const value = cacheMap.get(id), path = id + '.' + altKey;
+      if (value && value[altKey]) {
+        debug(`<< ${svcName} service hit cache`, path);
+        context.cacheHits = fp.concat(context.cacheHits || [], [path]);
+        return value[altKey];
+      } else {
+        debug(`<< ${svcName} service miss cache`, path);
+        return null;
+      }
+    };
+
     switch (context.method) {
-      case 'find': // fall through
+      case 'find':
+        if (context.params && context.params.query) {
+          const id = context.params.query.id || context.params.query._id || {};
+          if (fp.is(String, id)) {
+            const value = getFromCache(id);
+            if (value) {
+              context.result = resultFor([value]);
+            }
+          } else if (id.$in && id.$in.length > 0) {
+            const ids = fp.uniq(id.$in);
+            const values = fp.reject(fp.isNil, fp.map(getFromCache, ids));
+            if (values.length === ids.length) { // hit all
+              context.result = resultFor(values);
+            }
+          }
+        }
+        break;
       case 'create':
         break;
       case 'get': {
-        const value = cacheMap.get(context.id), path = context.id + '.' + altKey;
-        if (value && value[altKey]) {
-          debug(`<< ${svcName} service hit cache`, path);
-          context.cacheHits = fp.concat(context.cached || [], [path]);
-          context.result = value[altKey];
-        } else {
-          debug(`<< ${svcName} service miss cache`, path);
+        const value = getFromCache(context.id);
+        if (value) {
+          context.result = value;
         }
         break;
       }
