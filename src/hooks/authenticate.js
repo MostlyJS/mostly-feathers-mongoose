70c9607d638963e05d1d7e6f3cd48749470753d5
forafish <fora.luo@gmail.com>
2018-03-16 02:42:49 +0800

    remove anonymousToken and clean select fields code

--- src/hooks/authenticate.js
+++ src/hooks/authenticate.js
@@ -3,8 +3,6 @@ import { hooks as auth } from 'feathers-authentication';
 import { NotAuthenticated } from 'feathers-errors';
 import fp from 'mostly-func';
 
-const verifyIdentity = auth.authenticate('jwt');
-
 const defaultOptions = {
   anonymous: true,
 };
@@ -14,27 +12,24 @@ function getAccessToken(hook) {
          fp.path(['accessToken'], hook.data);
 }
 
-export default function authenticate(strategies, opts = {}) {
+export default function authenticate(strategies, opts = {}, fields = '') {
   opts = fp.assign(defaultOptions, opts);
   assert(strategies, "The 'authenticate' hook requires one of your registered passport strategies.");
 
+  const verifyIdentity = auth.authenticate(strategies);
+
   return async function(context) {
     const accessToken = getAccessToken(context);
-    const anonymousToken = fp.path(['auth', 'local', 'anonymousToken'], opts);
-    if (accessToken && anonymousToken && opts.anonymous && accessToken.startsWith(anonymousToken)) {
-      // create an anonymous user with provided fake accessToken like 'anonymous:uuid'
-      return context;
-    } else {
-      // verify and fetch user with $select fields
-      const permissionField = opts.permissionField || fp.path(['auth', 'local', 'permissionField'], opts);
-      if (permissionField) {
-        context.params = fp.assign(context.params, {
-          $auth: {
-            query: { $select: permissionField }
-          }
-        });
-      }
-      return await verifyIdentity(context);
+    // verify and fetch user with $select fields
+    const select = fields.split(',')
+      .concat(fp.pathOr([], ['local', 'fields'], opts)).join(',');
+    if (fields) {
+      context.params = fp.assign(context.params, {
+        $auth: {
+          query: { $select: select }
+        }
+      });
     }
+    return await verifyIdentity(context);
   };
 }
