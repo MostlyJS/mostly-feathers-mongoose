851f333bec9a20ad010612bc59ee789c0bd34f84
forafish <fora.luo@gmail.com>
2018-04-02 03:10:06 +0800

    add time log for esm loader

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 require = require("esm")(module/*, options*/);
+console.time('mostly-feathers-mongoose import');
 module.exports = require('./src/index').default;
+console.timeEnd('mostly-feathers-mongoose import');
