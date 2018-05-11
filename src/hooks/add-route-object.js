b192a16ea383361288b4993617f7369a5822900e
forafish <fora.luo@gmail.com>
2018-05-11 19:22:51 +0800

    Fix get route object with user for permission

--- src/hooks/add-route-object.js
+++ src/hooks/add-route-object.js
@@ -31,7 +31,10 @@ export default function addRouteObject (name, opts) {
     if (!context.params[name]) {
       try {
         const service = context.app.service(options.service);
-        const object = await service.get(primary, { query: { $select: opts.select }});
+        const object = await service.get(primary, {
+          query: { $select: opts.select },
+          user: context.params.user
+        });
         if (!object) {
           throw new Error(`Not found primary service object ${name} of ${primary}`);
         }
