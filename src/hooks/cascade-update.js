39c69ca6b6f6903c490664f172c662037346557e
forafish <fora.luo@gmail.com>
2018-05-17 01:34:49 +0800

    make async hook of cascadeUpdate

--- src/hooks/cascade-update.js
+++ src/hooks/cascade-update.js
@@ -11,32 +11,30 @@ export default function cascadeUpdate (target, opts) {
 
   var field = opts.field;
 
-  return function (hook) {
+  return async context => {
     let options = Object.assign({}, opts);
 
-    if (hook.type !== 'after') {
+    if (context.type !== 'after') {
       throw new Error(`The 'cascadeUpdate' hook should only be used as a 'after' hook.`);
     }
 
-    let data = hook.result && hook.result.data || hook.result;
+    let data = context.result && context.result.data || context.result;
     if (field && data) {
       let foreignField = get(data, target);
       if (data.id && foreignField) {
-        const service = hook.app.service(options.service);
+        const service = context.app.service(options.service);
         if (!service) {
           throw new Error("No such service: " + options.service);
         }
 
         let foreignId = getId(foreignField);
-        return service.patch(foreignId, {
+        await service.patch(foreignId, {
           [field]: options.value !== undefined? options.value : data.id.toString()
-        }).then(() => {
-          return hook;
         });
       } else {
         debug('Skip hook cascadeUpdate, nothing to run', target, field, data);
       }
     }
-    return hook;
+    return context;
   };
 }
